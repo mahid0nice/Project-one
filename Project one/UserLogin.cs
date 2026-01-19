6ffb4397cfc8ce0e204721c6d4863f94633421cc
Fahim Ahmed <352416fahimahmed@gmail.com>
2026-01-19 13:48:36 +0600

    showProfile() update

---
+++ Project one/UserLogin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_one
+{
+    internal static class UserLogin
+    {
+        //Arghya 
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Admin_Login());
+             // Application.Run(new Admin_Dashboard());
+            //Application.Run(new volunteer_login_page());
+            //Application.Run(new Volunteer_Dashboard());
+            //Application.Run(new Employee_Dashboard());
+        }
+    }
+}
